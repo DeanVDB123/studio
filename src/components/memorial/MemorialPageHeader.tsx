7ebf010bdfa2a767fb2371ceaf113ea81ef2f275
WeebieBean <deanvdberg28@gmail.com>
2025-06-28 18:05:01 +0000

    on mobile the app overlaps with stuff

--- src/components/memorial/MemorialPageHeader.tsx
+++ src/components/memorial/MemorialPageHeader.tsx
@@ -43,7 +43,7 @@ export function MemorialPageHeader({ deceasedName, birthDate, deathDate, lifeSum
         </Link>
       </div>
       <div className="container mx-auto px-4">
-        <h1 className="text-5xl md:text-6xl font-headline text-primary-foreground mb-3">{deceasedName}</h1>
+        <h1 className="text-4xl md:text-6xl font-headline text-primary-foreground mb-3">{deceasedName}</h1>
 
         {profilePhotoUrl && (
           <div className="my-6 flex justify-center">
